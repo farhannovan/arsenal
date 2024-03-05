72433779e6515121efb837fc686824c2d320ad8c
Farhan Novan <farhannovan@gmail.com>
2024-03-05 15:10:12 +0700

    first commit

---
+++ app/src/main/java/academy/bangkit/arsenal/ProfileActivity.kt
@@ -0,0 +1,17 @@
+package academy.bangkit.arsenal
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.ImageView
+
+class ProfileActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_profile)
+
+        val btnBack: ImageView = findViewById(R.id.ic_previous)
+        btnBack.setOnClickListener {
+            finish()
+        }
+    }
+}
